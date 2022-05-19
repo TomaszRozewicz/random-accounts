177989bbc5268942d56d0996e07d340900fd53cc
TomaszRozewicz <tsr1110@gmail.com>
2022-05-19 18:41:39 +0200

    Setup vue router

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router/index'
+import { createPinia } from 'pinia'
 import './index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(createPinia())
+
+router.isReady().then(() => {
+  app.mount('#app')
+})
