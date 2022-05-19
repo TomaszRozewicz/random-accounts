aaa814dca89abf1f2f05512a84dbcd0c13c2b363
TomaszRozewicz <tsr1110@gmail.com>
2022-05-19 19:05:11 +0200

    Setup app container and global components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,36 @@
+<script setup lang="ts">
+import AppPopup from './components/AppPopup.vue'
+import AppNotifications from './components/AppNotifications.vue'
+import { useBaseStore } from '@/stores/base'
+import { onBeforeMount } from 'vue'
+import { getSigner } from '@/utils/signer'
+
+const baseStore = useBaseStore()
+
+onBeforeMount(async () => {
+  try {
+    await getSigner()
+  } catch (error: any) {
+    baseStore.setPopupMessage(
+      'There is a problem with connection to your metamask wallet. Please make sure that you have metamask properly installed and refresh the page.'
+    )
+    console.error(error)
+  }
+})
+</script>
+
 <template>
-  <div>
-    vue 3
+  <AppNotifications v-if="baseStore.notifications.length" />
+
+  <AppPopup v-if="baseStore.popupMessage" />
+
+  <div
+    class="min-w-24 flex min-h-screen w-full items-center justify-center bg-gray-100"
+  >
+    <div class="flex max-w-full rounded-lg bg-white p-6 shadow">
+      <div>
+        <RouterView />
+      </div>
+    </div>
   </div>
 </template>
